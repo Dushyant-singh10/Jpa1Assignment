43ac5800f53cc8dbccbd9a59a6466faa5aede5df
Dushyant singh <ds0958570@gmail.com>
2026-02-20 23:12:35 +0530

    dependency add of lombook

--- SpringJpaAssignment1/src/main/java/com/example/SpringJpaAssignment1/Entities/Employee.java
+++ SpringJpaAssignment1/src/main/java/com/example/SpringJpaAssignment1/Entities/Employee.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.annotation.processing.Generated;
-
+@Setter
+@Getter
+@ToString
 @Entity
 public class Employee {
 
@@ -19,45 +24,4 @@ public class Employee {
 
 
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", location='" + location + '\'' +
-                '}';
-    }
 }
